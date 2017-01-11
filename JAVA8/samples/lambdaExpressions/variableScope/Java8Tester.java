f8f7a8780fdc011a94f513b3e010571a4b99ab60
Venkatesh Kolisetty <venky.svtv@live.com>
2017-01-11 22:33:07 +0530

    JAVA 8 Projects added
    Some of JAVA 8 projects done to test some functionalities

---
+++ JAVA8/samples/lambdaExpressions/variableScope/Java8Tester.java
@@ -0,0 +1,14 @@
+package lambdaExpressions.variableScope;
+
+public class Java8Tester {
+	final static String salutation = "Hello! ";
+
+	public static void main(String args[]) {
+		GreetingService greetService1 = message -> System.out.println(salutation + message);
+		greetService1.sayMessage("Mahesh");
+	}
+
+	interface GreetingService {
+		void sayMessage(String message);
+	}
+}
